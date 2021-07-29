396fdfef6808c6215147521cd35fea0ed30a2880
rcjdiv <62179956+rcjdiv@users.noreply.github.com>
2021-07-29 12:44:50 +0800

    Create main.py

---
+++ nestedIF/main.py
@@ -0,0 +1,39 @@
+print('''
+*****************************************
+⠁⠁⠁⠁⠁⠁⠐⢶⣶⣶⣶⣤⣤⡀⠁⠁⣠⣀⣀⠁⠁⠁⠁⠁⠁⠁⠁⠁⠁⠁
+⠁⠁⠁⠁⠁⠁⠁⠁⠙⢿⣯⣠⣶⣦⣤⣤⣌⣛⠻⢇⣠⣤⣤⠁⠁⠁⠁⠁⠁⠁
+⠁⠁⠁⠁⠁⠁⠁⠁⠁⠁⠻⣿⣿⣿⡟⢉⡤⢤⣤⣤⡍⠛⢡⢖⣥⣶⣦⣀⠁⠁
+⠁⠁⠁⠁⠁⠁⠁⠁⠁⠁⣠⣿⣿⣿⡏⣭⣶⣿⣿⠟⢿⣦⡡⣿⣿⡇⠁⡙⣷⡀
+⠁⠁⠁⠁⠁⠁⠁⣀⣴⣿⣿⣿⣿⣿⣿⡞⣿⣿⡟⢀⡀⣿⣿⢻⣿⣿⣀⣁⣿⠏
+⠁⠁⠁⢀⣠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣟⢰⢻⣿⣇⣈⣴⣿⠟⢨⣛⠛⠛⠉⠁⠁
+⠁⣠⣶⣿⣿⡟⢋⠤⣤⠘⢿⣿⣧⡙⠻⠌⠒⠙⠛⢛⣫⣥⣿⣦⡈⠉⣡⣴⣾⠇
+⢰⣿⣿⣿⣿⠁⡇⠁⠙⠷⣤⡙⠻⢿⣿⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⠿⠟⠋⠁⠁
+⠘⣿⣿⣿⣿⣆⠻⣄⠁⣀⡀⠉⠙⠒⠂⠉⠍⠉⠉⠉⠉⣩⣍⣁⣂⡈⠠⠂⠁⠁
+⠁⠘⢿⣿⣿⣿⣦⡉⠳⢬⣛⠷⢦⡄⠁⠁⠁⠁⠁⣀⣼⣿⣿⠿⠛⠋⠁⠁⠁⠁
+⠁⠁⠁⠉⠻⢿⣿⣿⣷⣦⣬⣍⣓⡒⠒⣒⣂⣠⡬⠽⠓⠂⠁⠁⠁⠁⠁⠁
+*****************************************
+''')
+print("Welcome to TEAM ROCKET fan Verification!.")
+print("Take the exam to verify.") 
+
+first = input('''
+Type "rocket" for team rocket or "ash" for team ash
+''')
+first_final = first.lower()
+
+if first_final == "rocket":
+  second = input('Type "xqc" for xqc type "myth" for Myth\n')
+  second_final = second.lower()
+  if second_final == "xqc":
+    third = input("Choose a colour among the three. red, yellow, and blue. Which colour do you choose?\n")
+    third_final = third.lower()
+    if third_final == "red":
+      print("You're officially part of Team Rocket")
+    elif third_final == "yellow":
+      print("You're an NPC")
+    elif third_final == "blue":
+      print("Go away dud")
+  else:
+    print("Fuck u Myth")  
+else:
+  print("Fuck u Ash")
