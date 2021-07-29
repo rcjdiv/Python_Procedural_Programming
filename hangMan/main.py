8ae841232ee6575a60d3ae9c467f9113edc619ff
rcjdiv <62179956+rcjdiv@users.noreply.github.com>
2021-07-29 12:38:33 +0800

    Create main.py

---
+++ hangMan/main.py
@@ -0,0 +1,52 @@
+#Step 5
+
+import random
+import hangman_art
+import hangman_words
+
+
+chosen_word = random.choice(hangman_words.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+
+#Testing code
+print(hangman_art.logo)
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if guess == display[position]:
+          print(f"You've already guessed {guess}")
+        elif letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(hangman_art.stages[lives])
